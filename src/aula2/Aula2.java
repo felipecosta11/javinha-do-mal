bbbf67b9977a213b71e1084e25c008c708b218fe
felipecosta11 <felipecosta75072@gmail.com>
2019-06-11 01:30:39 -0300

    aula

---
+++ src/aula2/Aula2.java
@@ -0,0 +1,13 @@
+package aula2;
+
+public class Aula2 {
+	public static void main(String args[]) {
+		int v = 10, n= 5, total=0;
+		total= v + n; 
+		System.out.println("Adição: " +	 (v+n)	   +	"Subtração: " +	 (v-n) +	"Multiplicação: " +		(v*n) +	"Divisão: " +	( v/n));
+	}
+}
+
+
+
+
